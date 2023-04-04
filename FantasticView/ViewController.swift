e3948a2e191165ce68507911ff1e6c2202018f08
Asha Patel <asha@hiehq.com>
2023-04-04 11:36:46 +0530

    latest code

--- FantasticView/ViewController.swift
+++ FantasticView/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         let fantasticView = FantasticView(frame: self.view.bounds)
                 
